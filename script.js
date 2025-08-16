dfc97ad214ec65bbb6ae0f76990c5f0ba85e539a
marcos-0215 <149538998+Marcos-0215@users.noreply.github.com>
2025-08-16 08:08:59 -0300

    backend connected

--- script.js
+++ script.js
@@ -1,29 +1,67 @@
 
-/****** POUXANDO LISTA DE NOMES DE PAISES ******/
+/****** PUXANDO LISTA DE NOMES DE PAISES ******/
 
-// Lista simplificada de países
+
+/*
 const countries = [
   { name: "Brasil", continent: "América do Sul", population: 213, capital: "Brasília" },
   { name: "Argentina", continent: "América do Sul", population: 45, capital: "Buenos Aires" },
   { name: "França", continent: "Europa", population: 67, capital: "Paris" },
   { name: "Japão", continent: "Ásia", population: 126, capital: "Tóquio" }
 ];
+*/
 
+const xhr = new XMLHttpRequest();
+let countries;
+let secret; // País Resposta
 
-const datalist = document.getElementById('countries-datalist');
+xhr.addEventListener('load', () => {
+  countries = JSON.parse(xhr.response);
+  //console.log(xhr.response);
+  //console.log(typeof countries);
 
-
-countries.forEach(c => {
-  const option = document.createElement('option');
-  option.value = c.name;
-  datalist.appendChild(option);
-
+  fillDatalist(countries);
+  draftSecret();
+  /*
+  countries.forEach(c => {
+    console.log(c.name.common);
+  });
+  */
 });
 
+xhr.open('GET', 'https://restcountries.com/v3.1/all?fields=name');
+xhr.send();
 
+
+
+
+
+function fillDatalist(list) {
+
+  const datalist = document.getElementById('countries-datalist');
+
+  list.sort((a, b) => {
+    return a.name.common.localeCompare(b.name.common)
+  });
+
+  list.forEach(c => {
+    const option = document.createElement('option');
+    option.value = c.name.common;
+    datalist.appendChild(option);
+    
+  });
+  
+}
 
 // Resposta -> um país aleatório 
-const secret = countries[Math.floor(Math.random() * countries.length)];
+function draftSecret() {
+  secret = countries[Math.floor(Math.random() * countries.length)];
+}
+
+
+
+
+
 
 
 const checkBtn = document.getElementById('check-guess-btn');
@@ -41,6 +79,7 @@ document.body.addEventListener('keydown', (event) => {
 let triesQnt = 0;
 
 
+
 function checkGuess() {
   const input = document.getElementById("guess-input").value.trim();
   const feedback = document.getElementById("feedback");
@@ -50,19 +89,23 @@ function checkGuess() {
 
   triesQnt++;
 
-  const guess = countries.find(c => c.name.toLowerCase() === input.toLowerCase());
+  const guess = countries.find(c => c.name.common.toLowerCase() === input.toLowerCase());
 
   if (!guess) {
     feedback.textContent = "❌ País não encontrado!";
     return;
   }
 
-  if (guess.name === secret.name) {
-    feedback.textContent = "🎉 Acertou! O país era " + secret.name;
+  //console.log(guess.name.common);
+  //console.log(secret.name.common);
+
+
+  if (guess.name.common === secret.name.common) {
+    feedback.textContent = "🎉 Acertou! O país era " + secret.name.common;
   } else {
     feedback.textContent = "🔎 Dica adicionada!";
     const li = document.createElement("li");
-    li.textContent = `Seu palpite: ${guess.name} | Continente: ${guess.continent} | População: ${guess.population}M | Capital: ${guess.capital}`;
+    li.textContent = `Seu palpite: ${guess.name.common} | Continente: ${guess.continent} | População: ${guess.population}M | Capital: ${guess.capital}`;
     history.appendChild(li);
   }
 
